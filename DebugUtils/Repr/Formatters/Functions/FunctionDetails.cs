b5b0ac232e058fbfb8040e857c31de01816b2b90
dj-lumiere <djminstar573@gmail.com>
2025-07-31 23:24:19 +0900

    fix: added data class for function repr

---
+++ DebugUtils/Repr/Formatters/Functions/FunctionDetails.cs
@@ -0,0 +1,65 @@
+﻿using System.Reflection;
+
+namespace DebugUtils.Repr.Formatters.Functions;
+
+public class FunctionDetails
+{
+    public string Name { get; }
+    public ParameterDetails[] Parameters { get; }
+    public ReturnInfo Return { get; }
+    public MethodModifiers Modifiers { get; }
+
+    public FunctionDetails(MethodInfo methodInfo)
+    {
+        Name = methodInfo.GetSanitizedName();
+        Parameters = methodInfo.GetParameterDetails();
+        Return = methodInfo.ToReturnInfo();
+        Modifiers = methodInfo.ToMethodModifiers();
+    }
+
+    public override string ToString()
+    {
+        return $"{Modifiers} {Return} {Name}({Parameters})";
+    }
+}
+
+internal static class FunctionDetailsExtensions
+{
+    public static FunctionDetails ToFunctionDetails(this MethodInfo methodInfo)
+    {
+        return new FunctionDetails(methodInfo);
+    }
+
+    public static string GetSanitizedName(this MethodInfo methodInfo)
+    {
+        var unsanitizedName = methodInfo.Name;
+        var sanitizedName = "";
+        if (unsanitizedName.Contains(value: "g__"))
+        {
+            // Since we are finding "g__" and "|", which consist of ascii character,
+            // it doesn't suffer from localization/cultural issues that matter how letters are counted.
+            var start = unsanitizedName.IndexOf("g__") + 3;
+            var end = unsanitizedName.IndexOf('|', start);
+            return end > start
+                ? unsanitizedName.Substring(start, end - start)
+                : "local func";
+        }
+
+        // lambda functions always contain "b__".
+        if (unsanitizedName.Contains(value: "b__"))
+        {
+            sanitizedName = "Lambda";
+            return sanitizedName;
+        }
+
+        return unsanitizedName;
+    }
+
+    public static ParameterDetails[] GetParameterDetails(
+        this MethodInfo methodInfo)
+    {
+        return methodInfo.GetParameters()
+                         .Select(selector: p => p.ToParameterDetails())
+                         .ToArray();
+    }
+}
---
+++ DebugUtils/Repr/Formatters/Functions/ReturnInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace DebugUtils.Repr.Formatters.Functions;
+
+public class ReturnInfo
+{
+    public string TypeName { get; }
+    public bool IsRef { get; }
+    public bool IsAsync { get; }
+
+    public ReturnInfo(MethodInfo method)
+    {
+        var returnType = method.ReturnType;
+        TypeName = returnType.GetReprTypeNameByTypeName();
+        IsRef = returnType.IsByRef;
+        IsAsync = method.IsDefined(typeof(AsyncStateMachineAttribute));
+    }
+
+    public override string ToString()
+    {
+        var result = IsRef
+            ? "ref "
+            : "";
+        result += IsAsync
+            ? $"async {TypeName}"
+            : TypeName;
+        return result;
+    }
+}
+
+internal static class ReturnInfoExtensions
+{
+    public static ReturnInfo ToReturnInfo(this MethodInfo methodInfo)
+    {
+        return new ReturnInfo(methodInfo);
+    }
+}
