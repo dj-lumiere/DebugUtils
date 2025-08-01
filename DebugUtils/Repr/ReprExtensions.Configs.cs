11e010f72e6c9725c9428dd4b2f0521fd4c31e8d
dj-lumiere <djminstar573@gmail.com>
2025-08-01 23:04:27 +0900

    refactor: moved the type name dictionaries to internal class TypeNameMappings

--- DebugUtils/Repr/ReprExtensions.Configs.cs
+++ DebugUtils/Repr/ReprExtensions.Configs.cs
@@ -4,40 +4,7 @@ namespace DebugUtils.Repr;
 
 public static partial class ReprExtensions
 {
-    public static readonly Dictionary<Type, string> CSharpTypeNames = new()
-    {
-        [key: typeof(void)] = "void",
-        [key: typeof(byte)] = "byte",
-        [key: typeof(sbyte)] = "sbyte",
-        [key: typeof(short)] = "short",
-        [key: typeof(ushort)] = "ushort",
-        [key: typeof(int)] = "int",
-        [key: typeof(uint)] = "uint",
-        [key: typeof(long)] = "long",
-        [key: typeof(ulong)] = "ulong",
-        [key: typeof(float)] = "float",
-        [key: typeof(double)] = "double",
-        [key: typeof(decimal)] = "decimal",
-        [key: typeof(object)] = "object"
-    };
 
-    public static readonly Dictionary<Type, string> FriendlyTypeNames = new()
-    {
-        [key: typeof(List<>)] = "List",
-        [key: typeof(Dictionary<,>)] = "Dictionary",
-        [key: typeof(HashSet<>)] = "HashSet",
-        [key: typeof(LinkedList<>)] = "LinkedList",
-        [key: typeof(Queue<>)] = "Queue",
-        [key: typeof(Stack<>)] = "Stack",
-        [key: typeof(SortedDictionary<,>)] = "SortedDictionary",
-        [key: typeof(SortedSet<>)] = "SortedSet",
-        [key: typeof(LinkedListNode<>)] = "LinkedListNode",
-        [key: typeof(KeyValuePair<,>)] = "KeyValuePair",
-        [key: typeof(ValueTuple<,>)] = "ValueTuple",
-        [key: typeof(char)] = "char",
-        [key: typeof(string)] = "string",
-        [key: typeof(bool)] = "bool"
-    };
 
     public static ReprConfig GetContainerConfig(this ReprConfig config)
     {
