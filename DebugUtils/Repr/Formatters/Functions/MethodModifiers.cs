cfc5ebd23e8da7cdbcd539ccc51ad9d43cc4d2b0
dj-lumiere <djminstar573@gmail.com>
2025-08-01 16:39:44 +0900

    refactor: moved helper methods to Extensions class

--- DebugUtils/Repr/Formatters/Functions/MethodModifiers.cs
+++ DebugUtils/Repr/Formatters/Functions/MethodModifiers.cs
@@ -21,105 +21,16 @@ public class MethodModifiers
 
     public MethodModifiers(MethodInfo method)
     {
-        AccessLevelName = GetAccessLevelName(method);
+        AccessLevelName = method.GetAccessLevelName();
         IsStatic = method.IsStatic;
-        IsVirtual = method.IsVirtual && !method.IsFinal; // Virtual but not sealed
-        IsOverride = IsOverrideMethod(method);
+        IsVirtual = method is { IsVirtual: true, IsFinal: false }; // Virtual but not sealed
+        IsOverride = method.IsOverrideMethod();
         IsAbstract = method.IsAbstract;
-        IsSealed = method.IsFinal && method.IsVirtual; // Sealed override
-        IsAsync = IsAsyncMethod(method);
+        IsSealed = method is { IsFinal: true, IsVirtual: true }; // Sealed override
+        IsAsync = method.IsAsyncMethod();
         IsGeneric = method.IsGenericMethod;
-        IsExtern = IsExternMethod(method);
-        IsUnsafe = IsUnsafeMethod(method);
-    }
-
-    private static string GetAccessLevelName(MethodInfo method)
-    {
-        if (method.IsPublic)
-        {
-            return "public";
-        }
-
-        if (method.IsPrivate)
-        {
-            return "private";
-        }
-
-        if (method.IsFamily)
-        {
-            return "protected";
-        }
-
-        if (method.IsAssembly)
-        {
-            return "internal";
-        }
-
-        if (method.IsFamilyOrAssembly)
-        {
-            return "protected internal";
-        }
-
-        if (method.IsFamilyAndAssembly)
-        {
-            return "private protected";
-        }
-
-        return "unknown";
-    }
-
-    private static bool IsOverrideMethod(MethodInfo method)
-    {
-        // A method is override if it's virtual and has a base definition
-        return method.IsVirtual &&
-               method.GetBaseDefinition() != method;
-    }
-
-    private static bool IsAsyncMethod(MethodInfo method)
-    {
-        // Check if method is marked with AsyncStateMachine attribute
-        return method.IsDefined(
-            attributeType: typeof(AsyncStateMachineAttribute));
-    }
-
-    private static bool IsExtensionMethod(MethodInfo method)
-    {
-        // Extension methods are static and have ExtensionAttribute
-        return method.IsStatic &&
-               method.IsDefined(attributeType: typeof(ExtensionAttribute));
-    }
-
-    private static bool IsOperatorMethod(MethodInfo method)
-    {
-        return method.IsSpecialName &&
-               (method.Name.StartsWith(value: "op_") || method.Name == "Implicit" ||
-                method.Name == "Explicit");
-    }
-
-    private static bool IsPropertyMethod(MethodInfo method)
-    {
-        return method.IsSpecialName &&
-               (method.Name.StartsWith(value: "get_") || method.Name.StartsWith(value: "set_"));
-    }
-    private static bool IsExternMethod(MethodInfo method)
-    {
-        return method.Attributes.HasFlag(flag: MethodAttributes.PinvokeImpl);
-    }
-    private static bool IsUnsafeMethod(MethodInfo method)
-    {
-        // A method is considered unsafe if its return type or any of its parameters are a pointer type.
-        if (method.ReturnType.IsPointer)
-        {
-            return true;
-        }
-
-        if (method.GetParameters()
-                  .Any(predicate: p => p.ParameterType.IsPointer))
-        {
-            return true;
-        }
-
-        return false;
+        IsExtern = method.IsExternMethod();
+        IsUnsafe = method.IsUnsafeMethod();
     }
 
     public override string ToString()
@@ -135,6 +46,7 @@ public class MethodModifiers
         // 2. Add Scope/Inheritance Modifiers
         // Note: C# syntax doesn't allow 'virtual' and 'abstract' with 'sealed'
         // so the order here handles the common combinations correctly.
+
         if (IsStatic)
         {
             modifiers.Add(item: "static");
@@ -191,5 +103,71 @@ internal static class MethodModifiersExtensions
     public static MethodModifiers ToMethodModifiers(this MethodInfo methodInfo)
     {
         return new MethodModifiers(method: methodInfo);
+    }
+    public static string GetAccessLevelName(this MethodInfo method)
+    {
+        if (method.IsPublic)
+        {
+            return "public";
+        }
+
+        if (method.IsPrivate)
+        {
+            return "private";
+        }
+
+        if (method.IsFamily)
+        {
+            return "protected";
+        }
+
+        if (method.IsAssembly)
+        {
+            return "internal";
+        }
+
+        if (method.IsFamilyOrAssembly)
+        {
+            return "protected internal";
+        }
+
+        if (method.IsFamilyAndAssembly)
+        {
+            return "private protected";
+        }
+
+        return "unknown";
+    }
+    public static bool IsOverrideMethod(this MethodInfo method)
+    {
+        // A method is override if it's virtual and has a base definition
+        return method.IsVirtual &&
+               method.GetBaseDefinition() != method;
+    }
+    public static bool IsAsyncMethod(this MethodInfo method)
+    {
+        // Check if method is marked with AsyncStateMachine attribute
+        return method.IsDefined(
+            attributeType: typeof(AsyncStateMachineAttribute));
+    }
+    public static bool IsExternMethod(this MethodInfo method)
+    {
+        return method.Attributes.HasFlag(flag: MethodAttributes.PinvokeImpl);
+    }
+    public static bool IsUnsafeMethod(this MethodInfo method)
+    {
+        // A method is considered unsafe if its return type or any of its parameters are a pointer type.
+        if (method.ReturnType.IsPointer)
+        {
+            return true;
+        }
+
+        if (method.GetParameters()
+                  .Any(predicate: p => p.ParameterType.IsPointer))
+        {
+            return true;
+        }
+
+        return false;
     }
 }
