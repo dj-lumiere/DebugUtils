8fa7c2169db0012da4399a54247b896cfb971a49
dj-lumiere <djminstar573@gmail.com>
2025-08-07 14:41:25 +0900

    feat: added priority queue support

---
+++ DebugUtils/Repr/Formatters/Collections/PriorityQueueFormatter.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Nodes;
+using DebugUtils.Repr.Attributes;
+using DebugUtils.Repr.Interfaces;
+using DebugUtils.Repr.Records;
+using DebugUtils.Repr.TypeHelpers;
+
+namespace DebugUtils.Repr.Formatters.Collections;
+
+[ReprOptions(needsPrefix: true)]
+internal class PriorityQueueFormatter : IReprFormatter, IReprTreeFormatter
+{
+    public string ToRepr(object obj, ReprContext context)
+    {
+        if (context.Config.MaxDepth >= 0 && context.Depth >= context.Config.MaxDepth)
+        {
+            return "<Max Depth Reached>";
+        }
+
+        var type = obj.GetType();
+        var countProperty = type.GetProperty(name: "Count");
+        var count = (int)(countProperty?.GetValue(obj: obj) ?? 0);
+
+        // Apply container defaults if configured
+        context = context.WithContainerConfig();
+
+        var unorderedItemsProperty = type.GetProperty(name: "UnorderedItems");
+        var unorderedItems = (IEnumerable)unorderedItemsProperty!.GetValue(obj: obj)!;
+
+        var items = new List<string>();
+        var i = 0;
+        var hitLimit = false;
+
+        foreach (var item in unorderedItems)
+        {
+            if (context.Config.MaxElementsPerCollection >= 0 &&
+                i >= context.Config.MaxElementsPerCollection)
+            {
+                hitLimit = true;
+                break;
+            }
+
+            if (item is ITuple tuple && tuple.Length == 2)
+            {
+                var priority = tuple[index: 1];
+                var element = tuple[index: 0];
+                items.Add(
+                    item:
+                    $"{element.Repr(context: context.WithIncrementedDepth())} (priority: {priority.Repr(context: context.WithIncrementedDepth())})");
+            }
+
+            i += 1;
+        }
+
+        if (hitLimit)
+        {
+            var remainingCount = count - context.Config.MaxElementsPerCollection;
+            if (remainingCount > 0)
+            {
+                items.Add(item: $"... ({remainingCount} more items)");
+            }
+            else
+            {
+                items.Add(item: "... (more items)");
+            }
+        }
+
+        return "[" + String.Join(separator: ", ", values: items) + "]";
+    }
+
+    public JsonNode ToReprTree(object obj, ReprContext context)
+    {
+        var type = obj.GetType();
+        var countProperty = type.GetProperty(name: "Count");
+        var count = (int)(countProperty?.GetValue(obj: obj) ?? 0);
+
+        if (context.Config.MaxDepth >= 0 && context.Depth >= context.Config.MaxDepth)
+        {
+            return new JsonObject
+            {
+                [propertyName: "type"] = type.GetReprTypeName(),
+                [propertyName: "kind"] = type.GetTypeKind(),
+                [propertyName: "maxDepthReached"] = "true",
+                [propertyName: "depth"] = context.Depth
+            };
+        }
+
+        var result = new JsonObject();
+        result.Add(propertyName: "type", value: type.GetReprTypeName());
+        result.Add(propertyName: "kind", value: type.GetTypeKind());
+        result.Add(propertyName: "hashCode", value: $"0x{RuntimeHelpers.GetHashCode(o: obj):X8}");
+        result.Add(propertyName: "count", value: count);
+
+        // Get generic type arguments for element and priority types
+        if (type.IsGenericType)
+        {
+            var genericArgs = type.GetGenericArguments();
+            if (genericArgs.Length >= 2)
+            {
+                result.Add(propertyName: "elementType", value: genericArgs[0]
+                   .GetReprTypeName());
+                result.Add(propertyName: "priorityType", value: genericArgs[1]
+                   .GetReprTypeName());
+            }
+        }
+
+        var entries = new JsonArray();
+        var unorderedItemsProperty = type.GetProperty(name: "UnorderedItems");
+        var unorderedItems = (IEnumerable)unorderedItemsProperty!.GetValue(obj: obj)!;
+
+        var i = 0;
+        var hitLimit = false;
+
+        foreach (var item in unorderedItems)
+        {
+            if (context.Config.MaxElementsPerCollection >= 0 &&
+                i >= context.Config.MaxElementsPerCollection)
+            {
+                hitLimit = true;
+                break;
+            }
+
+            if (item is ITuple tuple && tuple.Length == 2)
+            {
+                var priority = tuple[index: 1]
+                   .FormatAsJsonNode(context: context.WithIncrementedDepth());
+                var element = tuple[index: 0]
+                   .FormatAsJsonNode(context: context.WithIncrementedDepth());
+                var entry = new JsonObject();
+                entry.Add(propertyName: "element", value: element);
+                entry.Add(propertyName: "priority", value: priority);
+                entries.Add(value: entry);
+            }
+
+            i += 1;
+        }
+
+        if (hitLimit)
+        {
+            var remainingCount = count - context.Config.MaxElementsPerCollection;
+            if (remainingCount > 0)
+            {
+                entries.Add(value: $"... ({remainingCount} more items)");
+            }
+            else
+            {
+                entries.Add(value: "... (more items)");
+            }
+        }
+
+        result.Add(propertyName: "value", value: entries);
+        return result;
+    }
+}
